6423fc33dc5ed6b294a9522cbb276f75a928a668
Stefan Fisher <smfisheruga@gmail.com>
2014-03-10 20:43:30 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MiloAndLydia::Application.config.secret_key_base = '905f1f0313e865716d789b3a1ae15796350a3c40f51425f2a4aba3d09e399fd961247b7a9866dae5ef991e3ad455d1b3b9121c51834e706bf1cb65dd762d4768'
