3f767bd9049f760988d7791158c3986f739540b3
unknown <sfisher@CLWCXW1-SFISHER.medassets.com>
2014-03-17 17:21:30 -0400

    Still working on dropbox, going home

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,6 @@ MiloAndLydia::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  config.dropbox_key = {dropbox_key: ENV["dropbox"]}
 end
